60961c4c151d2d3bd4032a55f0295250bcd49237
Saadmamdouh123 <saadmamdouh6@gmail.com>
2025-07-02 16:59:40 +0100

    last modif

--- cha4.py
+++ cha4.py
@@ -1,9 +1,38 @@
-def factorial(liv):
-    fact = 1
-    for i in range(1, liv + 1):
-        fact*= i
-    return fact
-liv2 = int(input('enter a number'))
-print(factorial(liv2))
+# def factorial(liv):
+#     fact = 1
+#     for i in range(1, liv + 1):
+#         fact*= i
+#     return fact
+# liv2 = int(input('enter a number'))
+# print(factorial(liv2))
+#######################
+# def NumEnt(m):
+#     counter = 1
+#     while counter <= 10:
+#        result = counter * m 
+#        counter += 1
+#        print(result)
+
+# live = int(input('enter a number '))
+# print(NumEnt(live))
+#######################
+import math
+
+def CarreParfait():
+    try:
+        n = int(input('Entrez un nombre entier : '))
+        racine = math.sqrt(n)
+        if racine.is_integer():
+            print(n, 'est un carré parfait')
+        else:
+            print(n, 'n\'est pas un carré parfait')
+    except ValueError:
+        print("Veuillez entrer un entier valide.")
+
+CarreParfait()
+
+
+
+
     
 
