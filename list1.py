a245bf8dcaaed34b5020de0bfc090db2364135cb
Saadmamdouh123 <saadmamdouh6@gmail.com>
2025-07-02 21:20:21 +0100

    add list cha2

--- list1.py
+++ list1.py
@@ -1,13 +1,21 @@
 
-notes = [12, 4, 14, 11, 18, 13, 7, 10, 5, 9, 15, 8, 14, 16]
+# notes = [12, 4, 14, 11, 18, 13, 7, 10, 5, 9, 15, 8, 14, 16]
 
-moyenne = sum(notes) / len(notes)
+# moyenne = sum(notes) / len(notes)
 
-notes_sup = []
+# notes_sup = []
 
-for note in notes:
-    if note > moyenne:
-        notes_sup.append(note)
+# for note in notes:
+#     if note > moyenne:
+#         notes_sup.append(note)
 
-print(moyenne)
-print(notes_sup)
+# print(moyenne)
+# print(notes_sup)
+
+Ch1 = "Le langage Python est très populaire"
+Ch2 = "Python est un langage puissant"
+
+Ch1_words = set(Ch1.split())
+Ch2_words = set(Ch2.split())
+common = Ch1_words & Ch2_words
+print(common)
